1811175ac14bdf7a1e1c45d4d81d07ba3d3f0485
Alexey <a-nes.rnd@mai.ru>
2023-02-19 01:39:57 +0300

    Небольшие корректировки

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,5 +1,4 @@
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.utils.exceptions import Throttled
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
@@ -11,14 +10,15 @@ bot = Bot(token=token, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
-#Функция для защиты от флудил
+
+# Функция для защиты от флудил
 async def anti_flood(*args, **kwargs):
     pass
 
 
 # Это первоначальный опрос пользователя, чтобы узнать какую он информацию хочет получить
 @dp.message_handler(Text(equals=['/start', 'Назад', 'Вернуться к выбору информации']))
-@dp.throttled(anti_flood,rate=4)
+@dp.throttled(anti_flood, rate=4)
 async def start(message: types.Message):
     select_buttons = ['Учебные программы', 'Немного информации об Иннополисе']
     keyboard_start = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -32,7 +32,7 @@ async def start(message: types.Message):
 
 # Эта функция создана для того, чтобы пользователь мог выбрать по какой учебной программе он получит информацию
 @dp.message_handler(Text(equals=['Учебные программы']))
-@dp.throttled(anti_flood,rate=4)
+@dp.throttled(anti_flood, rate=4)
 async def choice_study_programs(message: types.Message):
     select_buttons = ['Бакалавриат', 'Магистратура', 'Аспирантура', 'Назад']
     keyboard_study_programs = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -46,7 +46,7 @@ async def choice_study_programs(message: types.Message):
 
 
 @dp.message_handler(Text(equals='Бакалавриат'))
-@dp.throttled(anti_flood,rate=4)
+@dp.throttled(anti_flood, rate=4)
 async def undergraduate(message: types.Message):
     await message.answer('Здесь будет информация о Бакалавриате')
 
@@ -54,7 +54,7 @@ async def undergraduate(message: types.Message):
 
 
 @dp.message_handler(Text(equals="Магистратура"))
-@dp.throttled(anti_flood,rate=4)
+@dp.throttled(anti_flood, rate=4)
 async def magistracy(message: types.Message):
     await message.answer('Здесь будет информация о Магистратуре')
 
@@ -62,14 +62,14 @@ async def magistracy(message: types.Message):
 
 
 @dp.message_handler(Text(equals="Аспирантура"))
-@dp.throttled(anti_flood,rate=4)
+@dp.throttled(anti_flood, rate=4)
 async def graduate_school(message: types.Message):
     await message.answer('Здесь будет информация о Аспирантуре')
 
 
 # Эта функция создана для выбора информации об иннополисе
 @dp.message_handler(Text(equals="Немного информации об Иннополисе"))
-@dp.throttled(anti_flood,rate=4)
+@dp.throttled(anti_flood, rate=4)
 async def study_information(message: types.Message):
     select_buttons = ["Об учебе в Иннополисе", "О городе Иннополис", "О кампусе", "О студенческой жизни", "Назад"]
     keyboard_study_information = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -78,32 +78,32 @@ async def study_information(message: types.Message):
 
 
 @dp.message_handler(Text(equals="Об учебе в Иннополисе"))
-@dp.throttled(anti_flood,rate=4)
+@dp.throttled(anti_flood, rate=4)
 async def about_study(message: types.Message):
     await message.answer(information_about_innopolis.about_study_parser())
 
 
 @dp.message_handler(Text(equals="О городе Иннополис"))
-@dp.throttled(anti_flood,rate=4)
+@dp.throttled(anti_flood, rate=4)
 async def about_the_city_of_innopolis(message: types.Message):
     await message.answer(information_about_innopolis.about_the_city_of_innopolis())
 
 
 @dp.message_handler(Text(equals="О кампусе"))
-@dp.throttled(anti_flood,rate=4)
+@dp.throttled(anti_flood, rate=4)
 async def about_campus(message: types.Message):
     await message.answer(information_about_innopolis.about_campus())
 
 
 @dp.message_handler(Text(equals="О студенческой жизни"))
-@dp.throttled(anti_flood,rate=4)
+@dp.throttled(anti_flood, rate=4)
 async def about_student_life(message: types.Message):
     await message.answer(information_about_innopolis.student_life())
 
 
 # Проверка сообщения, на то является ли оно командой или пользователь написал что-то странное
 @dp.message_handler(content_types=['text'])
-@dp.throttled(anti_flood,rate=4)
+@dp.throttled(anti_flood, rate=4)
 async def message_authentication(message: types.Message):
     if message.text in all_commands:
         pass
