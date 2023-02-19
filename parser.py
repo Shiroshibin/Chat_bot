a13353dcf5b65efba9051b8a00115d042f65c004
Alexey <a-nes.rnd@mai.ru>
2023-02-19 03:40:28 +0300

    Добавлены последние парсеры баклавриата и магистратуры - финал

--- parser.py
+++ parser.py
@@ -5,8 +5,9 @@ import lxml
 from re import sub as re_sub
 
 
-#Немного информации об Иннополисе
+# Немного информации об Иннополисе
 class information_about_innopolis():
+
     # О городе иннополис
     def about_the_city_of_innopolis():
         url = 'https://apply.innopolis.university/'
@@ -19,24 +20,28 @@ class information_about_innopolis():
 
         return city_title + '\n' + city_text
 
-
     # О кампусе
     def about_campus():
         url = 'https://apply.innopolis.university/'
-        response = requests.get(url)
+        try:
+            response = requests.get(url, headers={'User-Agent': ua.random}, timeout=3)
+        except TimeoutError:
+            return 'Похоже с сервером какие-то неполадки, пожалуйста попробуйте перезапустить бота с помощью это команды /start'
         soup = BeautifulSoup(response.text, 'lxml')
 
         campus = soup.find('div', 'campus-info-entrants-left')
-        campus_title_bad = campus.find('h2').text
+        campus_title = campus.find('h2').text
         campus_text = campus.find('p').text
 
-        return campus_title_bad.replace('                ', '') + '\n' + campus_text
-
+        return re_sub('\s{2,}', ' ', campus_title) + '\n' + campus_text
 
     # О студенческой жизни
     def student_life():
         url = 'https://apply.innopolis.university/'
-        response = requests.get(url)
+        try:
+            response = requests.get(url, headers={'User-Agent': ua.random}, timeout=3)
+        except TimeoutError:
+            return 'Похоже с сервером какие-то неполадки, пожалуйста попробуйте перезапустить бота с помощью это команды /start'
         soup = BeautifulSoup(response.text, 'lxml')
 
         student = soup.find('div', 'campus-info__student-life')
@@ -45,11 +50,13 @@ class information_about_innopolis():
 
         return student_title + '\n' + student_text
 
-
     # Эта функция отвечает за информацию об обучении
     def about_study_parser():
         url = "https://apply.innopolis.university/?lang=ru&id=12&site=s1&template=university24&landing_mode=edit"
-        response = requests.get(url)
+        try:
+            response = requests.get(url, headers={'User-Agent': ua.random}, timeout=3)
+        except TimeoutError:
+            return 'Похоже с сервером какие-то неполадки, пожалуйста попробуйте перезапустить бота с помощью это команды /start'
         soup = BeautifulSoup(response.text, 'lxml')
         list_of_training_information = soup.find_all("div", class_="three-card-slider-description-item__content")
         list_cards = ''
@@ -58,7 +65,7 @@ class information_about_innopolis():
         for information_card in list_of_training_information:
             card_title = information_card.find("h5", class_="three-card-slider-description-item-title").text
             card_text = information_card.find("p", class_="three-card-slider-description-item-text").text
-            list_cards += card_title + ':\n' + card_text.replace("            ", "") + '\n\n'
+            list_cards += card_title + ':\n' + re_sub('\s{2,}', ' ', card_text) + '\n\n'
 
         return list_cards
 
@@ -105,7 +112,7 @@ class postgraduate_studies():
         try:
             response = requests.get(url, headers={'User-Agent': ua.random}, timeout=3)
         except TimeoutError:
-            return 'проблемы с сервером'
+            return 'Похоже с сервером какие-то неполадки, пожалуйста попробуйте перезапустить бота с помощью это команды /start'
 
         soup = BeautifulSoup(response.text, 'lxml')
 
@@ -131,7 +138,7 @@ class postgraduate_studies():
         try:
             response = requests.get(url, headers={'User-Agent': ua.random}, timeout=3)
         except TimeoutError:
-            return 'проблемы с сервером'
+            return 'Похоже с сервером какие-то неполадки, пожалуйста попробуйте перезапустить бота с помощью это команды /start'
 
         soup = BeautifulSoup(response.text, 'lxml')
 
@@ -164,7 +171,7 @@ class postgraduate_studies():
         try:
             response = requests.get(url, headers={'User-Agent': ua.random}, timeout=3)
         except TimeoutError:
-            return 'проблемы с сервером'
+            return 'Похоже с сервером какие-то неполадки, пожалуйста попробуйте перезапустить бота с помощью это команды /start'
 
         soup = BeautifulSoup(response.text, 'lxml')
 
@@ -178,4 +185,160 @@ class postgraduate_studies():
         return all_learning_programs
 
 
+class magistr():
 
+    # направление подготовки
+    def direction_preparation():
+        url = 'https://apply.innopolis.university/master/datascience/?lang=ru&id=12&site=s1&template=university24&landing_mode=edit'
+        ua = UserAgent()
+
+        try:
+            response = requests.get(url, headers={'User-Agent': ua.random}, timeout=3)
+        except TimeoutError:
+            return 'Похоже с сервером какие-то неполадки, пожалуйста попробуйте перезапустить бота с помощью это команды /start'
+
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        # заголовок
+        direction_preparation_title = list(filter(lambda elem: elem,
+                                                  re_sub('ии', 'и и', soup.find("div", {
+                                                      "class": "main-page__main-viewport-titles"}).text).split('\n')))[
+            1]
+        # контент
+        direction_preparation_content = soup.find("div", class_="events__event-info__subtitle").text
+
+        # объединяем
+        all_direction = f'{direction_preparation_title}:\n{direction_preparation_content}'
+
+        return all_direction
+
+    # структура обучения
+    def education_schema():
+        url = 'https://apply.innopolis.university/master/datascience/?lang=ru&id=12&site=s1&template=university24&landing_mode=edit'
+        ua = UserAgent()
+
+        try:
+            response = requests.get(url, headers={'User-Agent': ua.random}, timeout=3)
+        except TimeoutError:
+            return 'Похоже с сервером какие-то неполадки, пожалуйста попробуйте перезапустить бота с помощью это команды /start'
+
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        # переменная для хранения всей структуры обучения
+        all_schema = ''
+
+        # все компоненты структуры
+        parts = soup.find("div", class_="learning-programs-wrap").find_all('div', {'class': 'learning-program'})[1:]
+        for part in parts:
+            # название
+            component = part.find("h5").text.capitalize().strip()
+            # подробное описание
+            description = re_sub('•', '\n•',
+                                 re_sub('\n', ' ', part.find("div",
+                                                             class_="learning-program-description").text)).strip().capitalize()
+            # объединяем
+            full_schema = f'{component}:\n{description}\n\n'
+            # добавляем к имеющейся части структуры
+            all_schema += full_schema
+
+        return all_schema
+
+    # учебные программы
+    def learning_programs():
+        url = 'https://apply.innopolis.university/?lang=ru&id=12&site=s1&template=university24&landing_mode=edit'
+        ua = UserAgent()
+
+        try:
+            response = requests.get(url, headers={'User-Agent': ua.random}, timeout=3)
+        except TimeoutError:
+            return 'Похоже с сервером какие-то неполадки, пожалуйста попробуйте перезапустить бота с помощью это команды /start'
+
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        # блок с учебными программами
+        learning_programs_div = soup.find('div', {'class': 'block-wrapper block-learning-programs'}).find('div', {
+            'class': 'learning-program master'})
+
+        # контент
+        all_learning_programs = '\n'.join([elem.text for elem in learning_programs_div.find_all('p')])
+
+        return all_learning_programs
+
+
+class bakalavr():
+
+    # структура обучения
+    def education_schema():
+        url = "https://apply.innopolis.university/bachelor/?lang=ru&id=12&site=s1&template=university24&landing_mode=edit"
+        ua = UserAgent()
+
+        try:
+            response = requests.get(url, headers={'User-Agent': ua.random}, timeout=3)
+        except TimeoutError:
+            return 'проблемы с сервером'
+
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        # переменная для хранения всей структуры обучения
+        all_schema = ''
+
+        # все компоненты структуры
+        parts = soup.find("div", class_="learning-programs-wrap").find_all('div', {'class': 'learning-program'})[1:]
+        for part in parts:
+            # название
+            component = part.find("h5").text.capitalize().strip()
+            # подробное описание
+            description = re_sub('•', '\n•',
+                                 re_sub('\n', ' ', part.find("div",
+                                                             class_="learning-program-description").text)).strip().capitalize()
+            # объединяем
+            full_schema = f'{component}:\n{description}\n\n'
+            # добавляем к имеющейся части структуры
+            all_schema += full_schema
+
+        return all_schema
+
+    # учебные программы
+    def learning_programs():
+        url = 'https://apply.innopolis.university/?lang=ru&id=12&site=s1&template=university24&landing_mode=edit'
+        ua = UserAgent()
+
+        try:
+            response = requests.get(url, headers={'User-Agent': ua.random}, timeout=3)
+        except TimeoutError:
+            return 'проблемы с сервером'
+
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        # блок с учебными программами
+        learning_programs_div = soup.find('div', {'class': 'block-wrapper block-learning-programs'}).find('div', {
+            'class': 'learning-program undergraduate'})
+
+        # контент
+        all_learning_programs = '\n'.join(
+            list(filter(lambda elem: elem, [elem.text for elem in learning_programs_div.find_all('p')]))
+        )
+
+        return all_learning_programs
+
+    # направление подготовки
+    def direction_preparation():
+        url = "https://apply.innopolis.university/bachelor/?lang=ru&id=12&site=s1&template=university24&landing_mode=edit"
+        ua = UserAgent()
+
+        try:
+            response = requests.get(url, headers={'User-Agent': ua.random}, timeout=3)
+        except TimeoutError:
+            return 'проблемы с сервером'
+
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        # заголовок
+        direction_preparation_title = soup.find("h3", {"class": "mainviewport-after-block__main-title"}).text
+        # контент
+        direction_preparation_content = soup.find("div", {"class": "mainviewport-after-block__main-subtitle"}).text
+
+        # объединяем
+        all_direction = f'{direction_preparation_title}:\n{direction_preparation_content}'
+
+        return all_direction
